6f25a3bba74b452e1860df50028c6ba3db996200
zhiyingzzhou <zhiyingzzhou@gmail.com>
2017-06-02 10:05:00 +0800

    新增data.js

---
+++ data.js
@@ -0,0 +1,98 @@
+let data = [
+    [
+        {
+            type:0,
+            text: 'AC'
+        },
+        {
+            type:0,
+            text: '+/-'
+        },
+        {
+            type:0,
+            text: '%'
+        },
+        {
+            type:0,
+            text: '÷'
+        }
+    ],
+    [
+        {
+            type:1,
+            text: '7',
+            num: 7
+        },
+        {
+            type:1,
+            text: '8',
+            num: 8
+        },
+        {
+            type:1,
+            text: '9',
+            num: 9
+        },
+        {
+            type:0,
+            text: 'x'
+        }
+    ],
+    [
+        {
+            type:1,
+            text: '4',
+            num: 4
+        },
+        {
+            type:1,
+            text: '5',
+            num: 5
+        },
+        {
+            type:1,
+            text: '6',
+            num: 6
+        },
+        {
+            type:0,
+            text: '-'
+        }
+    ],
+    [
+        {
+            type:1,
+            text: '1',
+            num: 1
+        },
+        {
+            type:1,
+            text: '2',
+            num: 2
+        },
+        {
+            type:1,
+            text: '3',
+            num: 3
+        },
+        {
+            type:0,
+            text: '+'
+        }
+    ],
+    [
+        {
+            type:1,
+            text: '0',
+            num: 0
+        },
+        {
+            type:0,
+            text: '.',
+        },
+        {
+            type:0,
+            text: '='
+        }
+    ],
+]
